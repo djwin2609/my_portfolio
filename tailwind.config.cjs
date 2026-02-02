7c3781fa45e0f9e8ff497b132888872ef8e9fb38
djwin2609 <nguyenhoangthienphuc09@gmail.com>
2026-02-02 18:01:25 +0700

    Fix tailwind config & add avatar

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,8 @@
 module.exports = {
+  darkMode: ["class"],
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
- 
